112c141f1578c94ad0abdf1061a9b3c46c224774
SUBHAJIT MONDAL <123165865+SitaMam@users.noreply.github.com>
2023-06-04 20:27:45 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,153 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+        body {
+            background: #F4F4F4;
+            margin: 0;
+            font-family: 'Quicksand-Regular';
+        }
+
+        #container {
+            width: 750px;
+            margin: 0 auto;
+            background: #FFFFFF;
+        }
+
+        #header {
+            padding: 45px 0 20px 40px;
+        }
+
+            #header a {
+                color: #121212;
+                text-decoration: none;
+                font-size: 30px;
+            }
+
+        #menu {
+            background-color: #3b7687;
+            padding: 6px 0 6px 40px;
+        }
+
+            #menu a {
+                color: #c5d6db;
+                text-decoration: none;
+                font-size: 14px;
+            }
+
+                #menu a:hover {
+                    color: #ecf2f3;
+                }
+
+        #sidebar {
+            float: left;
+            width: 120px;
+            padding: 30px 0 0 40px;
+            margin: 0;
+        }
+
+        h1 {
+            margin: 0px;
+            color: #869843;
+            font-size: 24px;
+            font-weight: normal;
+        }
+
+        h2 {
+            color: #3b7687;
+            font-size: 15px;
+            margin: 20px 0 5px 0;
+        }
+
+        #main {
+            margin: 0 0 0 180px;
+            padding: 35px 40px 30px 0;
+            color: #444444;
+            font-size: 13px;
+            line-height: 18px;
+            text-align: justify;
+        }
+
+            #main a {
+                color: #3b7687;
+                text-decoration: none;
+            }
+
+                #main a:hover {
+                    color: #444444;
+                }
+
+        #footer {
+            padding: 10px 50px 10px 50px;
+            background: #d6d6d6;
+            color: #444444;
+            font-size: 12px;
+            text-align: right;
+        }
+
+            #footer a {
+                text-decoration: none;
+                color: #262626;
+            }
+
+                #footer a:hover {
+                    color: #666666;
+                }
+
+        .separator {
+            font-size: 11px;
+            color: #FFFFFF;
+        }
+
+
+
+
+
+
+
+    </style>
+<title>Youtube to Mp3 - Most Powerful YouTube Converter</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+</head>
+<body>
+
+<div id="container">
+  <header>
+    <div id="header">
+  	<a href="https://www.convertmp3youtube.com/">Youtube to Mp3</a>
+    </div>
+    <div id="menu"> <a href="https://www.convertmp3youtube.com/">HOME</a> &nbsp; &nbsp; &nbsp; &nbsp; <a href="https://www.convertmp3youtube.com/">YouTube Converter</a>
+    </div>
+  </header>
+  <div id="sidebar">
+    <h1>Welcome!</h1>
+  </div>
+  <div id="main">
+    <p></p>
+    <h2>Most Powerful YouTube Converter</h2>
+    <p>Easily convert your YouTube videos to MP3 (audio) or MP4 (video) files using the fastest and most powerful YouTube Converter. No need for any applications or software downloads.
+    <br><br> <b>VISIT WEBSITE:</b> <br><br> <a href="https://www.convertmp3youtube.com/">https://www.convertmp3youtube.com/</a>
+    </p>
+  </div>
+  <footer>
+     <div id="footer"> &copy;2022 Youtube to Mp3 &nbsp;<span class="separator">|</span>&nbsp; Powered by <a href="https://www.convertmp3youtube.com/">Powerful YouTube Converter</a>
+     </div>
+  </footer>
+</div>
+</html>
